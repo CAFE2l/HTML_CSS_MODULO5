ce3dc122cb993abe03e5599be3691fbb638f5821
CAFE <pciskolargx@gmail.com>
2026-01-02 10:06:37 -0300

    css criado e exportado

---
+++ curso-react/src/Exercicios/ex027/flex014/index.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import '.index.css';
+export default function Flex002() {
+    
+
+  return (
+    <body> 
+      <header class="normal">
+        <h1>Exemplo de conteúdo flexível</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores ratione, culpa possimus similique officia quasi porro reiciendis ea ut dicta facere voluptates necessitatibus deleniti. Quos fuga dolores eius doloribus autem.</p>
+      </header>
+    <main>
+        <section class="flex-content">
+            <article class="media-text">
+                <div class="text">
+
+                </div>
+                <div class="media">
+
+
+                </div>
+            </article>
+            <article class="text-media">
+                <div class="text">
+
+                </div>
+                <div class="media">
+
+
+                </div>
+
+            </article>
+        </section>
+    
+        <section class="normal">
+            <h2>Conteúdo Normal</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates exercitationem numquam facere eligendi rerum quasi libero et, hic harum velit minima culpa sit in, dolorum nihil iure, molestias tempora! Aut. Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro laudantium, repellat recusandae rerum expedita id ratione quod deserunt facilis maiores sequi voluptates vitae, architecto, voluptas a! Fuga possimus soluta error!</p>
+        </section>
+    </main>
+
+    </body> 
+  );
+}
