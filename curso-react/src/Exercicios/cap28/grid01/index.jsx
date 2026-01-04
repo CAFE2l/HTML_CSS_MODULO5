8103e2b8139959d568c6a5c3c09bc461804817d0
CAFE2l <pciskolargx@gmail.com>
2026-01-04 11:01:44 -0300

    adicionando os grids

---
+++ curso-react/src/Exercicios/cap28/grid01/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './index.css';
+export default function  Flex002() {
+    
+
+  return (
+    <body> 
+      <div className="contaier">
+        <div className="item">A</div>
+        <div className="item">B</div>
+        <div className="item">C</div>
+        <div className="item">D</div>
+        <div className="item">E</div>
+        <div className="item">F</div>
+        <div className="item">G</div>
+      </div>
+
+    </body> 
+  );
+}
